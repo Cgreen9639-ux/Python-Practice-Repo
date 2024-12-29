d38abb703b5a1c989337c43119559ed4101fa552
Cameron Greene <125625082+Cgreen9639-ux@users.noreply.github.com>
2024-12-29 17:54:31 -0500

    Create _util.py
    Practice

---
+++ _util.py
@@ -0,0 +1,47 @@
+#These are small examples of me practicing straight python code (simply brushing up)//
+
+print(how many times does it happen)
+count(6*8+3) {
+Time it takes to go a lap around the track
+}
+
+#decimals
+
+x = 0.4
+y = -0.9
+int(x + y) = z
+isprintable(z = 1) 
+print(“this is a is decimal”))
+
+#itsdigit()
+
+num(20, 4, 6, 23, 45)
+
+#lstrip()
+
+Print(“Hello World”)
+lstrip() 
+
+small python exercise-
+istitle(Crime age)
+
+index(50, 48, 30, 70)
+
+tuple(1:3)
+rindex(index)
+print(Ages of John Hopkins Patients in order) {
+
+“With the ages of these patients it shows that in Baltimore crime is happening with middle aged people today on 12/28”
+}
+X = average age of patients
+x= (‘(“50*48*30*70”)/4)’)
+print(x) {
+Average age of people is 
+}
+String(‘Orange, Apple , Bannana’) 
+rjust(string)
+
+translate(160.0lbs to kg)
+terminal = blank kg
+
+#1 hour of practice 
